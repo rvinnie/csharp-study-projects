89562b9f647f4ffe5d671b2fad8b1ed46a4e22f3
rvinnie <rv1nnie@yandex.ru>
2022-01-31 18:51:01 +0300

    d01 ex00 done

---
+++ d01/d01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+using d01.Models;
+
+namespace d01
+{
+	class Program
+	{
+		public const string ERROR_MSG = "Ошибка ввода. Проверьте входные данные и повторите запрос.";
+
+		static void Main(string[] args)
+		{
+			CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+			if (args.Length != 2)
+			{
+				Console.WriteLine(ERROR_MSG);
+				return;
+			}
+
+			try
+			{
+				Exchanger exchanger = new Exchanger(args[1]);
+				List<ExchangerSum> convertedCurrency = new List<ExchangerSum>();
+				var sourceCurrency = new ExchangerSum(args[0]);
+				var convertedCurrencies = exchanger.Exchange(sourceCurrency);
+				Console.WriteLine("Сумма в исходной валюте: " + sourceCurrency);
+				foreach (var element in convertedCurrencies)
+				{
+					Console.WriteLine("Сумма в " + element.Currency + ": " + element);
+				}
+			}
+			catch
+			{
+				Console.WriteLine(ERROR_MSG);
+			}
+		}
+	}
+}
