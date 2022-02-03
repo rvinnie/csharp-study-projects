97e313de5dc2eb5f19bd2a0f8439de9722a3b587
Alexey Fedoseev <rv1nnie@yandex.ru>
2022-02-03 16:21:32 +0300

    rename folder d01 -> d01_ex01

---
+++ d01/d01_ex00/Exchanger.cs
@@ -0,0 +1,42 @@
+﻿using d01.Models;
+
+namespace d01
+{
+	class Exchanger
+	{
+		public List<ExchangerRate> RateList { get; private set; }
+		public List<string> KnownCurrencies { get; private set; }
+
+		public Exchanger(string pathToRates)
+		{
+			RateList = new List<ExchangerRate>();
+			KnownCurrencies = new List<string>();
+			foreach (var fullFileName in Directory.GetFiles(pathToRates))
+			{
+				string fileName = Path.GetFileNameWithoutExtension(fullFileName);
+				KnownCurrencies.Add(fileName);
+				string[] lines = File.ReadAllLines(fullFileName);
+				foreach (var line in lines)
+				{
+					RateList.Add(new ExchangerRate(fileName, line));
+				}
+			}
+		}
+
+		public IEnumerable<ExchangerSum> Exchange(ExchangerSum currencyToChange)
+		{
+			if (!KnownCurrencies.Contains(currencyToChange.Currency))
+				throw new ArgumentException();
+			return ExchangeIterator(currencyToChange);
+		}
+
+		public IEnumerable<ExchangerSum> ExchangeIterator(ExchangerSum currencyToChange)
+		{
+			foreach (var rate in RateList)
+			{
+				if (rate.FromCurrency == currencyToChange.Currency)
+					yield return new ExchangerSum(currencyToChange.Sum * rate.Rate, rate.ToCurrency);
+			}
+		}
+	}
+}
